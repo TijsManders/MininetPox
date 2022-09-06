e60ba2f6f5eb2cbbddf70c32f8822177687cbcb8
Tijs Manders <tijs.manders@student.fontys.nl>
2022-09-06 14:53:45 +0200

    gepersonaliseerd

--- tree.py
+++ tree.py
@@ -6,54 +6,36 @@ from mininet.log import setLogLevel, info
 def treeTopo():
     net = Mininet( controller=RemoteController )
     
-    info( '*** Adding controller\n' )
-    net.addController('c0')
+    info( 'Controller wordt toegevoegd\n' )
+    net.addController('c1')
     
-    info( '*** Adding hosts\n' )
+    info( 'Hosts worden toegevoegd\n' )
     h1 = net.addHost( 'h1', ip='10.0.0.1', mac='00:00:00:00:00:01' )
     h2 = net.addHost( 'h2', ip='10.0.0.2', mac='00:00:00:00:00:02' )
     h3 = net.addHost( 'h3', ip='10.0.0.3', mac='00:00:00:00:00:03' )
-    h4 = net.addHost( 'h4', ip='10.0.0.4', mac='00:00:00:00:00:04' )
-    h5 = net.addHost( 'h5', ip='10.0.0.5', mac='00:00:00:00:00:05' )
-    h6 = net.addHost( 'h6', ip='10.0.0.6', mac='00:00:00:00:00:06' )
-    h7 = net.addHost( 'h7', ip='10.0.0.7', mac='00:00:00:00:00:07' )
-    h8 = net.addHost( 'h8', ip='10.0.0.8', mac='00:00:00:00:00:08' )
 
-    info( '*** Adding switches\n' )
+    info( 'Switches worden toegevoegd\n' )
     s1 = net.addSwitch( 's1' )
     s2 = net.addSwitch( 's2' )
-    s3 = net.addSwitch( 's3' )
-    s4 = net.addSwitch( 's4' )
-    s5 = net.addSwitch( 's5' )
-    s6 = net.addSwitch( 's6' )
-    s7 = net.addSwitch( 's7' )
     
-    info( '*** Creating links\n' )
-    net.addLink( h1, s3 )
-    net.addLink( h2, s3 )
-    net.addLink( h3, s4 )
-    net.addLink( h4, s4 )
-    net.addLink( h5, s6 )
-    net.addLink( h6, s6 )
-    net.addLink( h7, s7 )
-    net.addLink( h8, s7 )
-    
+    info( 'Verbindingen worden toegevoegd\n' )
+    net.addLink( h1, s1 )
+    net.addLink( h2, s1 )
+    net.addLink( h3, s2 )
+   
     root = s1
-    layer1 = [s2,s5]
-    layer2 = [s3,s4,s6,s7]
+    layer1 = [s1,s2]
     
     for idx,l1 in enumerate(layer1):
         net.addLink( root,l1 )
-        net.addLink( l1, layer2[2*idx] )
-        net.addLink( l1, layer2[2*idx + 1] )
         
-    info( '*** Starting network\n')
+    info( 'Netwerk wordt gestart\n')
     net.start()
     
-    info( '*** Running CLI\n' )
+    info( 'CLI wordt gestart\n' )
     CLI( net )
     
-    info( '*** Stopping network' )
+    info( 'Netwerk wordt gestopt' )
     net.stop()
     
 if __name__ == '__main__':
